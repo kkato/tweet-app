bbb050e92c05cfffcbf314c837c1a033634d39e0
kkato <kkato149@gmail.com>
2024-11-07 21:34:59 +0900

    seventh part done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "default_user.jpg"
+    )
     if @user.save
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
@@ -25,11 +29,17 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
-  # updateアクションを作成してください
   def update
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+
     if @user.save
       flash[:notice] = "ユーザー情報を編集しました"
       redirect_to("/users/#{@user.id}")
@@ -37,4 +47,5 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end
+
 end
